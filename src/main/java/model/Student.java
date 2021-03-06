0883fd0329c02529f40730d73e9f3db0bd587c4e
Rakib <rakibccj@gmail.com>
2021-03-06 18:47:13 +0600

    Added JsonIgnoreProprties for Ignore the unknown properties

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,5 +1,8 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Student {
     private int id;
     private String firstName;
